1299d103dfc7e398a56783d14547c6af5c165cc5
Harman Deep Singh <harman88157@gmail.com>
2026-09-22 23:03:33 +0530

    refactor(backend): remove Express implementation

--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-import app from "./app";
-
-dotenv.config();
-
-const port = Number(process.env.PORT) || 3000;
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server running on port ${port}`);
-});
